58fca5d968cd8bfa96b4346024adae35320ae3b6
sollankie <ignat.pauliuchenka@yandex.ru>
2024-11-13 14:30:54 +0300

    Initial commit: Setup project with React, TypeScript, SCSS, and components

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/Header.scss';
+
+const Header: React.FC = () => {
+  return (
+    <header>
+      <h1>Мое резюме</h1>
+    </header>
+  );
+};
+
+export default Header;
