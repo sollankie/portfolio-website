ed75780fa449dd2f4f0d78edd5f8f2b3be2434e4
sollankie <ignat.pauliuchenka@yandex.ru>
2024-11-17 00:57:02 +0300

    Fix horizontal layout for navigation links

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -17,15 +17,13 @@ const Navigation: React.FC = () => {
         </li>
         <li>
           <Link to="skills" smooth={true} duration={500} className={styles.link}>Навыки</Link>
-        </li> 
+        </li>
         <li>
           <Link to="experience" smooth={true} duration={500} className={styles.link}>Опыт</Link>
         </li>
         <li>
           <Link to="projects" smooth={true} duration={500} className={styles.link}>Проекты</Link>
         </li>
-       
-        
       </ul>
     </nav>
   );
