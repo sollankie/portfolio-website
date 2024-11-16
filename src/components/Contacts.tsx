1c0156885e25bb706d98580776fea612b70b8ce5
sollankie <ignat.pauliuchenka@yandex.ru>
2024-11-17 01:07:14 +0300

    Add phone number with icon and adjust contact layout

--- src/components/Contacts.tsx
+++ src/components/Contacts.tsx
@@ -1,21 +1,37 @@
 import React from "react";
-import styles from '../styles/Contacts.module.scss';
+import { FaPhone, FaEnvelope, FaGithub, FaLinkedin } from "react-icons/fa"; // Иконки
+import styles from "../styles/Contacts.module.scss";
 
 const Contacts: React.FC = () => {
-    return (
-        <section id='Contacts' className={styles.container}>
-            <h2 className={styles.title}>Контакты</h2>
-            <ul className={styles.list}>
-                <li>Email: <a href="mailto:ignat.pauliuchenka@yandex.ru">ignat.pauliuchenka@yandex.ru</a></li>
-                <li>GitHub: <a href="https://github.com/sollankie" 
-                                target="_blank" 
-                                rel="noopener noreferrer">sollankie</a></li>
-                <li>LinkedIn: <a href="https://www.linkedin.com/in/ihnat-pauliuchenka-095a991b9/"
-                                        target="_blank"
-                                        rel="noopener noreferrer">Ihnat Pauliuchenka</a></li>
-            </ul>
-        </section>
-    )
-} 
+  return (
+    <section id="contacts" className={styles.container}>
+      <h2 className={styles.title}>Контакты</h2>
+      <ul className={styles.list}>
+        <li>
+          <FaPhone />
+          <a href="tel:+375297266305">+375 29 726 63 05</a>
+        </li>
+        <li>
+          <FaEnvelope />
+          <a href="mailto:ignat.pauliuchenka@yandex.ru">ignat.pauliuchenka@yandex.ru</a>
+        </li>
+        <li>
+          <FaGithub />
+          <a href="https://github.com/sollankie" target="_blank" rel="noopener noreferrer">
+            Sollankie
+          </a>
+        </li>
+        <li>
+          <FaLinkedin />
+          <a href="https://www.linkedin.com/in/ihnat-pauliuchenka-095a991b9/" target="_blank" rel="noopener noreferrer">
+            Ihnat Pauliuchenka
+          </a>
+        </li>
+      </ul>
+    </section>
+  );
+};
 
 export default Contacts;
+
+
